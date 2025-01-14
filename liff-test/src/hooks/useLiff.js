3a95ff4ced7dc6f05d575fab3d484ee00aaf5b3a
Futamase Yuki <futamase_yuki@cagamefi.co.jp>
2025-01-15 06:43:34 +0900

    update: react表示

--- liff-test/src/hooks/useLiff.js
+++ liff-test/src/hooks/useLiff.js
@@ -4,6 +4,7 @@ import { liff } from '../lib/liff';
 function useLiff({ liffId }) {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [profile, setProfile] = useState(null);
 
   const initLiff = async ({ liffId }) => {
     setLoading(true);
@@ -13,6 +14,19 @@ function useLiff({ liffId }) {
       alert('success liff init');
     } catch (error) {
       alert({ error });
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const fetchProfile = async () => {
+    setLoading(true);
+    try {
+      // LIFF APIのgetProfileを実行し結果をセット
+      setProfile(await liff.getProfile());
+    } catch (error) {
+      console.log({ error });
       setError(error);
     } finally {
       setLoading(false);
